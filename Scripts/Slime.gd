f6dbe6a2d7147cbda86e49ad000ac4622580b668
Theo Reeves <reevest@unc.edu>
2024-06-01 16:03:03 +0200

    Basic Slime + Movement added

---
+++ Scripts/Slime.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+@onready var Player = get_node("/root/World/Game Manager/Player")
+@export var speed = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _physics_process(delta):
+	if (update_velocity() > 10):
+		move_and_slide()
+
+
+func update_velocity():
+	var target = Player.get_global_position()
+	velocity = position.direction_to(target) * speed
+	return position.distance_to(target)
