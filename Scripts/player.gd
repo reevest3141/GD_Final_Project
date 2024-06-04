ed4dd9da8eff35e62cf3e23a2a53b359646ca60f
Theo Reeves <reevest@unc.edu>
2024-06-04 11:13:30 +0200

    Sprite update

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 @onready var current_hp = total_hp
 @export var attack_dmg = 3
 var gold = 0
+var anim_frames = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,35 +20,31 @@ func _physics_process(delta):
 	animate_player()
 	handle_input()
 	move_and_slide()
+	if anim_frames > 0:
+		anim_frames -= delta
 	
 func animate_player():
-	#if velocity.length() == 0:
-		#animations.stop()
-	#else:
-		#var direction = "_down"
-		#if velocity.x > 0:
-			#direction = "_right"
-		#elif velocity.x < 0:
-			#direction = "_left"
-		#elif velocity.y < 0:
-			#direction = "_up"
-		#animations.play("walk" + direction)
-	pass
-	
+	animations.flip_h = (velocity.x < 0)
+	if anim_frames > 0:
+		return
+	if velocity.length() == 0:
+		animations.play("Idle")
+	else:
+		animations.play("Walk")
 	
 func handle_input():
 	var direction = Input.get_vector("Left","Right","Up","Down")
 	velocity = direction * speed
-	if velocity.x < 0:
-		scale.x = -1
-	else:
-		scale.x = 1
 		
 	if Input.is_action_just_pressed("Attack"):
+		animations.play("Attack_01")
+		anim_frames = 6
 		pass
 
 
 func take_damage(dmg):
+	animations.play("Hurt")
+	anim_frames = 4
 	current_hp -= dmg
 	if current_hp <= 0:
 		die()
