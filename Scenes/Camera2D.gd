ecacd884fa17d12ff430995f32ad75fbf5325409
Zachary Wachtel <waza@ad.unc.edu>
2024-06-04 15:35:54 +0200

    he

---
+++ Scenes/Camera2D.gd
@@ -0,0 +1,10 @@
+extends Camera2D
+const LEVEL_1 = preload("res://Scenes/level_1.tscn/")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
