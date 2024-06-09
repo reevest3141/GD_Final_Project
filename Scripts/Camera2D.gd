ca5dcceda47b6aad9b1d8fb1b1426f608ba41a08
Sam Bradin <sbradin03@gmail.com>
2024-06-09 15:23:09 +0200

    camera shake added

--- Scripts/Camera2D.gd
+++ Scripts/Camera2D.gd
@@ -4,8 +4,19 @@ extends Camera2D
 func _ready():
 	pass
 
+var shake_amount = 0
+var shake_timer = 0
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if shake_timer > 0:
+		shake_timer -= delta
+		global_position = get_parent().global_position + Vector2(randf_range(-shake_amount, shake_amount), randf_range(-shake_amount, shake_amount))
+	#else:
+		#global_position = player.global_position
+
+func start_shake(amount, duration):
+	shake_amount = amount
+	shake_timer = duration
+
+func randf_range(min, max):
+	return randf() * (max - min) + min
